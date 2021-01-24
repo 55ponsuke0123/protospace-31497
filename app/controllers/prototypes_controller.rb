62464d2486984dbcb2b4a66b54342de09290c5ab
55ponsuke0123 <74514503+55ponsuke0123@users.noreply.github.com>
2021-01-25 00:30:28 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, expect: [:index, :show] 
+  before_action :move_to_index, except: [:index, :show]
+ 
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -26,18 +28,16 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to action: :index
+    unless @prototype.user_id == current_user.id
+      redirect_to root_path
     end
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-    if prototype.save
-      redirect_to prototype_path
-    else
-      @prototype = @user.prototype.includes(:user)
+    @prototype = Prototype.find(params[:id])
+     if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+     else
       render :edit
     end
   end
@@ -48,10 +48,19 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
+
+
   private
+
   def prototype_params
     params[:prototype].permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
   
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
   
+
 end
